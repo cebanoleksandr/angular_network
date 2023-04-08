8a7b182508e27111e4de3d77774aa9d18433c248
alex <cebanoleksandr@gmail.com>
2023-04-08 11:47:48 +0300

    add disabled property to follow button

--- Angular/my_projects/angular_network/src/app/components/people/user/user.component.ts
+++ Angular/my_projects/angular_network/src/app/components/people/user/user.component.ts
@@ -11,24 +11,28 @@ export class UserComponent {
   
   @Input() user!: User;
 
-  followed: boolean = this.user?.followed;
+  isFetching: boolean = false;
 
   constructor(
     private peopleService: PeopleService
   ) {}
 
   follow(id: number) {
+    this.isFetching = true;
     this.peopleService.addFriend(id).subscribe((response) => {
       if (response.resultCode === 0) {        
         this.user.followed = true;
+        this.isFetching = false;
       }
     })
   }
 
   unfollow(id: number) {
+    this.isFetching = true;
     this.peopleService.removeFriend(id).subscribe((response) => {
       if (response.resultCode === 0) {        
         this.user.followed = false;
+        this.isFetching = false;
       }
     })
   }
